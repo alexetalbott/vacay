f412699cef17bcbb30aaefe3048296bf10750543
Alex Talbott <alex.e.talbott@gmail.com>
2018-02-24 14:56:53 -0600

    make progress with demostrating branching in results and results content

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   $("#quiz form").submit(function(event) {
     var yourNameInput = $("input#yourName").val();
+    var customName = $("input:radio[name=customName]:checked").val();
+    var yourNewName = yourNameInput.concat(customName);
+
+    $(".yourNewName").text(yourNewName);
 
 
-
-  $("results").show();
+  $("results1").show();
 event.preventDefault();
   });
 
