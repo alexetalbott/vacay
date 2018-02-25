967fe859ee80de773b263960d19d8111978a6752
Alex Talbott <alex.e.talbott@gmail.com>
2018-02-25 12:50:47 -0600

    add more possible vacation results & begin branching

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,15 @@ $(document).ready(function() {
   $("#quiz form").submit(function(event) {
     var yourNameInput = $("input#yourName").val();
     var customName = $("input:radio[name=customName]:checked").val();
-    var yourNewName = yourNameInput.concat(customName);
+    var yourNewName = yourNameInput.concat(customName)+"!";
+    var favoriteColor = $("#color").val();
+    var favoriteLocation = $("input:radio[name=location]:checked").val();
 
     $(".yourNewName").text(yourNewName);
 
+    if ()
+    $("#results1").show();
 
-  $("results1").show();
 event.preventDefault();
   });
 
