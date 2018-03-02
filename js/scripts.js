0acb30504a28d0dafef14dbb3a90a30feb872232
Alex Talbott <alex.e.talbott@gmail.com>
2018-03-01 20:56:12 -0600

    add fourth choice, add complexity to javascript branching, add london image

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,33 @@ $(document).ready(function() {
     var customName = $("input:radio[name=customName]:checked").val();
     var yourNewName = yourNameInput.concat(customName)+"!";
     var favoriteColor = $("#color").val();
+    var tropicalCultural = $("input:radio[name=tropicalChoice]:checked").val();
     var favoriteLocation = $("input:radio[name=location]:checked").val();
 
     $(".yourNewName").text(yourNewName);
 
     event.preventDefault();
 
-    if (favoriteLocation === 'Dominican Republic') {
+    if (favoriteLocation === 'Dominican Republic' && tropicalCultural === 'tropical') {
       $("#results2").hide()
       $("#results3").hide();
+      $("#results4").hide();
       $("#results1").show();
-    } else if (favoriteLocation === 'Amalfi Coast') {
+    } else if (favoriteLocation === 'Amalfi Coast'&& tropicalCultural === 'tropical') {
       $("#results1").hide();
       $("#results3").hide();
+      $("#results4").hide();
       $("#results2").show();
+    } else if (favoriteLocation === 'The Great Barrier Reef' && tropicalCultural === 'tropical') {
+      $("#results1").hide();
+      $("#results2").hide();
+      $("#results4").hide();
+      $("#results3").show();
     } else {
       $("#results1").hide();
       $("#results2").hide();
-      $("#results3").show();
+      $("#results3").hide();
+      $("#results4").show();
     }
   });
 
